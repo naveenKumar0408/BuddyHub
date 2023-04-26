0993d51c857ce4932594f1de667ef5164f84631c
naveenKumar0408 <naveenkumar19981119@gmail.com>
2023-04-26 15:46:15 +0530

    Add Routing and Bootswatch theme to Client app

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +11,19 @@ export class RegisterComponent {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
   register() {
     this.accountService.register(this.model).subscribe({
       next: () => {
         this.cancel();
       },
-      error: (error) => console.log(error),
+      error: (error) => {
+        this.toastr.error(error.error);
+        console.log(error);
+      },
     });
   }
   cancel() {
