0cd96e5d27d5bb58839af5cd54828b40e83dfd3f
naveenKumar0408 <naveenkumar19981119@gmail.com>
2023-05-10 15:27:29 +0530

    Add Code to Update,add loader and save state

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -30,6 +31,18 @@ namespace API.Controllers
         [HttpGet("{username}")]
          public async Task<ActionResult<MemberDto>> GetUser(string username){
             return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            if(user == null) return NotFound();
+            _mapper.Map(memberUpdateDto,user);
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
